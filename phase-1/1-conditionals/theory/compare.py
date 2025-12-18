04cbd57b73b01ec36a29ed4473dcd55076d2b85f
Jiaks <160535015+Jiaks95@users.noreply.github.com>
2025-12-18 22:45:07 +0100

    Phase 1 Week 1: Complete lecture notes on Conditionals

--- phase-1/1-conditionals/theory/compare.py
+++ phase-1/1-conditionals/theory/compare.py
@@ -21,5 +21,16 @@ def main_2():
     else:
         print(f"{x} is equal to {y}")
 
+def main_3():
+    x = int(input("What's x? "))
+    y = int(input("What's y? "))
+
+    if x != y:
+        print(f"{x} is not equal to {y}")
+
+    else:
+        print(f"{x} is equal to {y}")
+
 main()
+main_2()
 main_2()
---
+++ phase-1/1-conditionals/theory/house.py
@@ -0,0 +1,14 @@
+def main():
+    name = input("What's your name: ").strip().title()
+
+    match name:
+        case "Harry" | "Hermione" | "Ron": 
+            print("Gryffindor")
+        
+        case "Draco":
+            print("Slytherin")
+
+        case _:
+            print("Who?")
+
+main()
